3825d91159ff3fee72022f5380a9931b3fa556ba
liuyi <3319233753@qq.com>
2026-04-05 15:36:22 +0800

    新增Data注解

--- src/main/java/org/liuyi/file/api/DownloadFileRequest.java
+++ src/main/java/org/liuyi/file/api/DownloadFileRequest.java
@@ -1,5 +1,8 @@
 package org.liuyi.file.api;
 
+import lombok.Data;
+
+@Data
 public class DownloadFileRequest {
     String fileId;
 }
--- src/main/java/org/liuyi/file/api/DownloadFileResponse.java
+++ src/main/java/org/liuyi/file/api/DownloadFileResponse.java
@@ -1,5 +1,8 @@
 package org.liuyi.file.api;
 
+import lombok.Data;
+
+@Data
 public class DownloadFileResponse extends BaseResponse {
     // 新增错误类型枚举
     public enum DownloadErrorType {
