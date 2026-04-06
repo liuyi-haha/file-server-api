2c72fe76934848f0dabd49c2da139bbd6e446a03
liuyi <3319233753@qq.com>
2026-04-06 23:37:19 +0800

    将请求和响应都变成可序列化的

--- src/main/java/org/liuyi/file/api/BaseResponse.java
+++ src/main/java/org/liuyi/file/api/BaseResponse.java
@@ -4,11 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 
 @Data
-@Builder
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 public class BaseResponse implements Serializable {
